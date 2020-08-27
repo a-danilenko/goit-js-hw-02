a419bb998bfdaccc8892de48170c250d02ea1470
a-danilenko <adanilenko7@gmail.com>
2020-08-27 15:42:35 +0300

    Update task-2-01.js

--- js/task-2-01.js
+++ js/task-2-01.js
@@ -1,10 +1,34 @@
-const logItems = function (array) {
-  // твой код
+// const logItems = function (array) {
+//   let result = " ";
+
+//   for (let i = 0; i < array.length; i += 1) {
+//     result = "${i} - ${array[i + 1]}\n";
+//   }
+
+//   return result;
+// };
+
+// logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
+
+// logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+// console.log(result);
+
+
+const getItemsString = function (array) {
+  'use strict';
+
+  // let result = '';
+
+  // for (let i = 0; i < array.length; i += 1) {
+  //   // result += '${i} - ${array[i+1]}\n';
+  // }
+
+  // return result;
 };
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
-logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
 
-logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+//console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
+
+
+console.log(getItemsString([5, 10, 15]));
