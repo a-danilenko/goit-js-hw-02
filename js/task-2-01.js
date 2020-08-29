b24d5a5db2364e4c044193d5752b8d15519b21b0
a-danilenko <adanilenko7@gmail.com>
2020-08-29 10:55:18 +0300

    hw2.1

--- js/task-2-01.js
+++ js/task-2-01.js
@@ -1,34 +1,16 @@
-// const logItems = function (array) {
-//   let result = " ";
-
-//   for (let i = 0; i < array.length; i += 1) {
-//     result = "${i} - ${array[i + 1]}\n";
-//   }
-
-//   return result;
-// };
-
-// logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
-
-// logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
-
-// console.log(result);
-
-
 const getItemsString = function (array) {
   'use strict';
+  let result = '';
 
-  // let result = '';
+  for (let i = 0; i < array.length; i += 1) {
+    result += `${i + 1} - ${array[i]}\n`;
+  }
 
-  // for (let i = 0; i < array.length; i += 1) {
-  //   // result += '${i} - ${array[i+1]}\n';
-  // }
-
-  // return result;
+  return result;
 };
 
 
-//console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
+console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
 
 
 console.log(getItemsString([5, 10, 15]));
